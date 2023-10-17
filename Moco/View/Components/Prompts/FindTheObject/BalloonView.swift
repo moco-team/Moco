49711df9f1a92cc7c39d36d55bcf304e9bc97250
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-10-17 23:00:38 +0700

    feat: complete logic find the object

---
+++ Moco/View/Components/Prompts/FindTheObject/BalloonView.swift
@@ -0,0 +1,82 @@
+//
+//  BalloonView.swift
+//  Moco
+//
+//  Created by Carissa Farry Hilmi Az Zahra on 17/10/23.
+//
+
+import SwiftUI
+
+struct Balloon {
+    let color: String
+    let isCorrect: Bool
+}
+
+struct BalloonView: View {
+    @Environment(\.audioViewModel) private var audioViewModel
+    let balloon: Balloon
+
+    @Binding var correctTryCount: Int
+    @Binding var falseTryCount: Int
+    @Binding var isFinalPopUp: Bool
+    @Binding var showTheAnswer: Bool
+    @Binding var isCorrectBalloonTapped: Bool
+    
+    @State private var isAnimating = false
+    @State private var isHidden = false
+    
+    var body: some View {
+        VStack {
+            if !isHidden {
+                Image("Story/Content/Story1/Pages/Page1/balon-\(balloon.color)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .opacity(isAnimating ? 0.0 : 1.0)
+                    .onTapGesture {
+                        if balloon.isCorrect {
+                            audioViewModel.playSound(soundFileName: "success")
+                            correctTryCount+=1
+                            print("Benar \(correctTryCount)")
+                            
+                            withAnimation(.easeOut(duration: 0.5)) {
+                               self.isAnimating = true
+                            }
+                            
+                            if correctTryCount < 3 {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    isCorrectBalloonTapped = true
+                                }
+                            }
+                            else {
+                                isFinalPopUp = true
+                            }
+                        } 
+                        else {
+                            if falseTryCount > 2 {
+                                showTheAnswer = true
+                            } else {
+                                falseTryCount += 1
+                                print("Salah \(falseTryCount)")
+                                audioViewModel.playSound(soundFileName: "balon_merah")
+                                isCorrectBalloonTapped = false
+                            }
+                        }
+                            
+                        
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    BalloonView(
+        balloon: Balloon(color: "orange", isCorrect: true),
+        correctTryCount: .constant(0),
+        falseTryCount: .constant(0),
+        isFinalPopUp: .constant(false),
+        showTheAnswer: .constant(false),
+        isCorrectBalloonTapped: .constant(false)
+    )
+}
--- Moco/View/Components/Prompts/FindTheObject/FindTheObject.swift
+++ Moco/View/Components/Prompts/FindTheObject/FindTheObject.swift
@@ -7,60 +7,67 @@
 
 import SwiftUI
 
-struct Balloon {
-    let color: String
-    let isCorrect: Bool
-}
-
-
 struct FindTheObjectView: View {
     @Environment(\.navigate) private var navigate
     
     @Binding var isPromptDone: Bool
     
     let content: String
-    let balloons: [Balloon]
+    let hints: [String]
+    let correctAnswer: String
+    @State var balloons: [Balloon]
     
-    @State private var isPromptVisible = false
-    @State private var isCorrectObjectTapped = false
+    let maxTry = 3
+    
+    @State private var correctTryCount = 0
+    @State private var falseTryCount = 0
+    @State private var isTried = false
+    @State private var showTheAnswer = false
+    @State private var isCorrectBalloonTapped = false
+    @State private var isAnimating = false
+    @State private var isFinalPopUp = false
+    
+    @State private var showTheBalloons = true
     
     var body: some View {
         VStack {
-            ForEach(balloons, id: \.color) { balloon in
-                Button(action: {
-                    handleObjectTap(balloon: balloon)
-                }) {
-                    Text(balloon.color)
-                        .font(.subheadline)
-                        .padding()
-                        .background(balloon.isCorrect ? Color.green : Color.blue)
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                    
-                    Image("Story/Content/Story1/Pages/Page1/balon-\(balloon.color)").resizable().frame(width: 22, height: 22)
+            if showTheBalloons {
+                HStack(spacing: 100) {
+                    ForEach(balloons.indices, id: \.self) { index in
+                        BalloonView(
+                            balloon: balloons[index],
+                            correctTryCount: $correctTryCount,
+                            falseTryCount: $falseTryCount,
+                            isFinalPopUp: $isFinalPopUp,
+                            showTheAnswer: $showTheAnswer,
+                            isCorrectBalloonTapped: $isCorrectBalloonTapped
+                        )
+                    }
                 }
+                .padding(.horizontal, Screen.width * 0.1)
+                .opacity(showTheBalloons ? 1.0 : 0.0)
+                .animation(.easeOut)
             }
         }
-        .popUp(isActive: $isCorrectObjectTapped, title: "Selamat kamu suda menemukan balon yang Moco cari! \n Lanjutkan cerita?") {
+        .padding(.vertical, 60)
+        .popUp(isActive: $isCorrectBalloonTapped, title: "Selamat kamu sudah menemukan balon yang Moco cari! \n Ayo cari balon selanjutnya!") {
+            if correctTryCount < maxTry {
+                isTried = false
+                isCorrectBalloonTapped = false
+                balloons.shuffle()
+            } else {
+                isPromptDone = true
+                print(isPromptDone)
+                print("Selesai!")
+            }
+        }
+        .popUp(isActive: $showTheAnswer, title: correctAnswer) {
             isPromptDone = true
-            print(isPromptDone)
+            print("Selesai!")
         }
-        
-        // Prompt to find the correct object
-        if isPromptVisible {
-            Text(isCorrectObjectTapped ? "Correct!" : "Try again.")
-                .font(.headline)
-                .foregroundColor(isCorrectObjectTapped ? .green : .red)
+        .popUp(isActive: $isFinalPopUp, title: "Selamat kamu berhasil mencari semua balon Moco!") {
+            isPromptDone = true
         }
-    }
-    
-    func handleObjectTap(balloon: Balloon) {
-        if balloon.isCorrect {
-            isCorrectObjectTapped = true
-        } else {
-            isCorrectObjectTapped = false
-        }
-        isPromptVisible = true
     }
 }
 
@@ -68,10 +75,12 @@ struct FindTheObjectView: View {
     FindTheObjectView(
         isPromptDone: .constant(false),
         content: "Once upon a time...",
+        hints: ["Coba lagi!", "Ayo coba lagi!"],
+        correctAnswer: "Jawaban yang benar adalah balon berwarna Merah",
         balloons: [
             Balloon(color: "orange", isCorrect: false),
-            Balloon(color: "ungu", isCorrect: true),
-            Balloon(color: "merah", isCorrect: false),
+            Balloon(color: "ungu", isCorrect: false),
+            Balloon(color: "merah", isCorrect: true),
             Balloon(color: "hijau", isCorrect: false),
             Balloon(color: "biru", isCorrect: false)
         ]
