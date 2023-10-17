ff145af7ab5e0d6aa80cf23f8680615bbfb6dc92
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2023-10-17 15:47:11 +0700

    feat: audio env

--- Moco/Extension/EnvironmentValue.swift
+++ Moco/Extension/EnvironmentValue.swift
@@ -9,10 +9,19 @@ import Foundation
 import SwiftUI
 
 extension EnvironmentValues {
+    var audioViewModel: AudioViewModel {
+        get { self[AudioViewModelKey.self] }
+        set { self[AudioViewModelKey.self] = newValue }
+    }
     var itemViewModel: ItemViewModel {
         get { self[ItemViewModelKey.self] }
         set { self[ItemViewModelKey.self] = newValue }
     }
+}
+
+
+private struct AudioViewModelKey: EnvironmentKey {
+    static var defaultValue: AudioViewModel = .init()
 }
 
 private struct ItemViewModelKey: EnvironmentKey {
