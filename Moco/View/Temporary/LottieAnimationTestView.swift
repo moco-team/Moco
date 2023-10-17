33c29fe227082915cf93ca951d3200ed3d8b571b
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-10-17 15:56:12 +0700

    feat: Make Lottie Animation

---
+++ Moco/View/Temporary/LottieAnimationTestView.swift
@@ -0,0 +1,21 @@
+//
+//  LottieAnimationTestView.swift
+//  Moco
+//
+//  Created by Daniel Aprillio on 17/10/23.
+//
+
+import SwiftUI
+
+struct LottieAnimationTestView: View {
+    var body: some View {
+        VStack{
+            LottieView(fileName: "testing-cat.json", width: 75, height: 75)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    LottieAnimationTestView()
+}
