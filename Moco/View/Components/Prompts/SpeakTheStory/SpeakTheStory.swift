a13546cfbacaa49cd8793517b60c41f1af7e6dfd
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-10-20 00:21:20 +0700

    feat: add speak story (unfinished)

---
+++ Moco/View/Components/Prompts/SpeakTheStory/SpeakTheStory.swift
@@ -0,0 +1,37 @@
+//
+//  SpeakTheStory.swift
+//  Moco
+//
+//  Created by Carissa Farry Hilmi Az Zahra on 19/10/23.
+//
+
+import SwiftUI
+
+struct SpeakTheStory: View {
+    @ObservedObject var speechRecognizerViewModel = SpeechRecognizerViewModel.shared
+    
+    @State private var isRecording = false
+    
+    var body: some View {
+        Text("Transcribed Text: \(speechRecognizerViewModel.transcript)")
+            .foregroundStyle(.gray)
+            .padding()
+        
+        Button(action: {
+            if isRecording {
+                speechRecognizerViewModel.stopTranscribing()
+            } else {
+                speechRecognizerViewModel.transcribe()
+            }
+            isRecording.toggle()
+        }) {
+            Text(isRecording ? "Stop Transcription" : "Start Transcription")
+                .font(.title)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    SpeakTheStory()
+}
