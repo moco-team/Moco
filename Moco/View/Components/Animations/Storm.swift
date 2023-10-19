15d35330ab9ea62d2133d332ad4bd4aaecc771f1
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2023-10-19 14:01:07 +0700

    chore: rename variable

--- Moco/View/Components/Animations/Storm.swift
+++ Moco/View/Components/Animations/Storm.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct Raindrop: Hashable, Equatable {
-    var x: Double
+    var xPosition: Double
     var removalDate: Date
     var speed: Double
 }
@@ -29,7 +29,7 @@ class Storm: ObservableObject {
 
     func update(to date: Date) {
         drops = drops.filter { $0.removalDate > date }
-        drops.insert(Raindrop(x: Double.random(in: 0 ... 1), removalDate: date + 1, speed: Double.random(in: 1 ... 2)))
+        drops.insert(Raindrop(xPosition: Double.random(in: 0 ... 1), removalDate: date + 1, speed: Double.random(in: 1 ... 2)))
     }
 }
 
@@ -43,7 +43,7 @@ struct StormView: View {
 
                 for drop in storm.drops {
                     let age = timeline.date.distance(to: drop.removalDate)
-                    let rect = CGRect(x: drop.x * size.width, y: size.height - (size.height * age * drop.speed), width: 2, height: 10)
+                    let rect = CGRect(x: drop.xPosition * size.width, y: size.height - (size.height * age * drop.speed), width: 2, height: 10)
                     let shape = Capsule().path(in: rect)
                     context.fill(shape, with: .color(storm.rainColor))
                 }
