a0e88daa1a71eb7ccafffd6be500f2dc6dfa27dc
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2023-10-30 23:33:18 +0700

    refactor: migrate states to view model

--- Moco/View/Components/Prompts/Maze/MazePrompt.swift
+++ Moco/View/Components/Prompts/Maze/MazePrompt.swift
@@ -8,60 +8,36 @@
 import SwiftUI
 
 struct MazePrompt: View {
-    @State private var blurOpacity = 0.0
-    @State private var showStartButton = false
-    @State private var isStarted = false
+    @State private var mazePromptViewModel = MazePromptViewModel()
 
     var promptText = "Moco adalah sapi jantan"
-
-    func playPrompt() {
-        isStarted = false
-        withAnimation(.easeInOut(duration: 3)) {
-            blurOpacity = 1
-        } completion: {
-            withAnimation(.easeInOut.delay(2)) {
-                showStartButton = true
-            }
-        }
-    }
-
-    func stopPrompt() {
-        withAnimation(.easeInOut(duration: 2)) {
-            blurOpacity = 0
-            showStartButton = false
-        } completion: {
-            withAnimation {
-                isStarted = true
-            }
-        }
-    }
 
     var body: some View {
         ZStack {
             MazeView()
 
-            if !isStarted {
+            if !mazePromptViewModel.isStarted {
                 VStack {
                     Text(promptText)
                         .customFont(.didactGothic, size: 30)
                         .foregroundColor(.text.primary)
-                        .opacity(blurOpacity)
-                    if showStartButton {
+                        .opacity(mazePromptViewModel.blurOpacity)
+                    if mazePromptViewModel.showStartButton {
                         Button("Mulai") {
-                            stopPrompt()
+                            mazePromptViewModel.stopPrompt()
                         }
                         .buttonStyle(
                             MainButton(width: 80, height: 20)
                         )
                         .padding(.bottom, 20)
                     }
-                }.frame(width: Screen.width, height: Screen.height).background(.ultraThinMaterial.opacity(blurOpacity))
+                }.frame(width: Screen.width, height: Screen.height).background(.ultraThinMaterial.opacity(mazePromptViewModel.blurOpacity))
             } else {
                 VStack {
                     HStack {
                         Spacer()
                         Button {
-                            playPrompt()
+                            mazePromptViewModel.playPrompt()
                         } label: {
                             Image(systemName: "arrow.counterclockwise.circle")
                                 .resizable()
@@ -75,7 +51,7 @@ struct MazePrompt: View {
                 }
             }
         }.onAppear {
-            playPrompt()
+            mazePromptViewModel.playPrompt()
         }
     }
 }
