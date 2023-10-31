31141dc4c96e12560af4740226529459c4e5f15e
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-10-31 11:18:36 +0700

    feat: add tutorial components

---
+++ Moco/View/Components/Tutorial/TutorialCard.swift
@@ -0,0 +1,54 @@
+//
+//  TutorialCard.swift
+//  Moco
+//
+//  Created by Aaron Christopher Tanhar on 31/10/23.
+//
+
+import SwiftUI
+
+struct TutorialCard<Content: View>: View {
+    @State var width: CGFloat = 300
+    @State var height: CGFloat = 300
+    @State var cornerRadius: CGFloat = 20
+    @State var backgroundColorTop: Color = Color.clear
+    @State var backgroundColorBottom: Color = Color.clear
+    @State var borderColor: Color = Color.gray
+    @State var backgroundImage: String
+    var content: () -> Content?
+    
+    init(width: CGFloat = 300, height: CGFloat = 300, cornerRadius: CGFloat = 20, backgroundColorTop: Color = Color.clear, backgroundColorBottom: Color = Color.clear, borderColor: Color = Color.white, backgroundImage: String = "", @ViewBuilder component: @escaping () -> Content?) {
+        self.width = width
+        self.height = height
+        self.cornerRadius = cornerRadius
+        self.backgroundColorTop = backgroundColorTop
+        self.backgroundColorBottom = backgroundColorBottom
+        self.borderColor = borderColor
+        self.backgroundImage = backgroundImage
+        self.content = component
+    }
+    
+    var body: some View {
+        VStack(){
+            if (content != nil) {
+                content()
+            }
+        }
+        .frame(width: width, height: height).overlay(
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .stroke(lineWidth: 2)
+                .foregroundColor(borderColor)
+        )
+        .background(LinearGradient(colors: [backgroundColorTop, backgroundColorBottom],
+                                    startPoint: .top,
+                                    endPoint: .center))
+        .cornerRadius(cornerRadius)
+    }
+}
+
+struct TutorialCard_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorialCard(component: {VStack{}})
+    }
+}
+
