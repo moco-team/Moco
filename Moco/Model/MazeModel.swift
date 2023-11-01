84a05a8e086e6da64c3d6dfb16df6d92cb12acb5
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-11-01 17:05:16 +0700

    fix: change maze size
    Co-Authored-By: Aaron Christopher <me@aaronct.dev>

--- Moco/Model/MazeModel.swift
+++ Moco/Model/MazeModel.swift
@@ -30,7 +30,7 @@ struct MazeModel {
     var startPoint = LocationPoint()
     var exitPoints = [LocationPoint()]
 
-    static var mapSize = MapSize(width: 37, height: 25)
+    static var mapSize = MapSize(width: 35, height: 25)
 
     var arrayPoint: [[Int]] = [[]]
 
