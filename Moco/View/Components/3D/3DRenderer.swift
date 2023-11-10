17fc888d2f20c8ef3026cab29e35fb6095af1b20
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2023-11-10 14:16:06 +0700

    feat: 3d environment scenekit

---
+++ Moco/View/Components/3D/3DRenderer.swift
@@ -0,0 +1,18 @@
+//
+//  3DRenderer.swift
+//  Moco
+//
+//  Created by Aaron Christopher Tanhar on 10/11/23.
+//
+
+import SwiftUI
+
+struct _DRenderer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _DRenderer()
+}
